6347bae934e2673cd829290ea570986436b8e1f0
Benedict022 <137492762+Benedict022@users.noreply.github.com>
2024-06-20 00:07:00 +0800

    Update HelloWorld17.sol

--- HelloWorld17.sol
+++ HelloWorld17.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.9;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Module4Degen is ERC20 {
+contract Alcantara is ERC20 {
     address public owner;
 
     struct Item {
@@ -20,32 +20,54 @@ contract Module4Degen is ERC20 {
     Item[] public itemsList;
     mapping(address => Item[]) public RedeemedItemsList;
     mapping(address => User) public users;
+    mapping(address => bool) public frozenAccounts;
+
+    event Freeze(address indexed account);
+    event Unfreeze(address indexed account);
 
     constructor(address _owner) ERC20("Degen", "DGN") {
         require(_owner != address(0), "Owner address cannot be the zero address");
         owner = _owner;
 
-        //Genshin Skin [itemID, itemName, itemCost, itemStock]
         itemsList.push(Item(0, "Blessing of the Welkin Moon", 50, 50));
         itemsList.push(Item(1, "Gnostic Chorus", 60, 50));
         itemsList.push(Item(2, "Ganyu: Twilight Blossom Skin", 150, 10));
         itemsList.push(Item(3, "Jean: Sea Breeze Dandelion Skin", 200, 5));
-        itemsList.push(Item(4, "Diluc: Red Dead of the Night Skin", 500, 3));
+        itemsList.push(Item(4, "Diluc: Red Dead of the Night Skin", 500, 1));
     }
 
-    function mint(address account, uint256 amount) public {
-        require(msg.sender == owner, "Only the owner can use this mint function");
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can perform this action");
+        _;
+    }
+
+    modifier notFrozen(address account) {
+        require(!frozenAccounts[account], "Account is frozen");
+        _;
+    }
+
+    function freezeAccount(address account) public onlyOwner {
+        frozenAccounts[account] = true;
+        emit Freeze(account);
+    }
+
+    function unfreezeAccount(address account) public onlyOwner {
+        frozenAccounts[account] = false;
+        emit Unfreeze(account);
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner notFrozen(account) {
         _mint(account, amount);
     }
 
-    function burn(address account, uint256 amount) public {
+    function burn(address account, uint256 amount) public notFrozen(account) {
         require(amount > 0, "Burn amount must be greater than zero");
         require(account != address(0), "Account address cannot be the zero address");
         require(balanceOf(account) >= amount, "Insufficient balance to burn");
         _burn(account, amount);
     }
 
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
+    function transfer(address recipient, uint256 amount) public override notFrozen(msg.sender) notFrozen(recipient) returns (bool) {
         require(amount > 0, "Transfer amount must be greater than zero");
         return super.transfer(recipient, amount);
     }
@@ -58,37 +80,15 @@ contract Module4Degen is ERC20 {
         return RedeemedItemsList[account];
     }
 
-    function redeem(uint256 itemId, address account) public {
+    function redeem(uint256 itemId, address account) public notFrozen(account) {
         require(itemId < itemsList.length, "Invalid item ID");
         Item storage item = itemsList[itemId];
         require(balanceOf(account) >= item.cost, "Insufficient balance to redeem item");
         require(item.supply > 0, "Item is out of stock");
         require(account != address(0), "Account address cannot be the zero address");
 
-        _transfer(account, owner, item.cost);
+        burn(account, item.cost);
         RedeemedItemsList[account].push(item);
         item.supply--;
-    }
-
-    function Upgrade_Membership(address account) public {
-        require(account != address(0), "Account address cannot be the zero address");
-        require(balanceOf(account) >= 100, "Insufficient balance to upgrade membership");
-        
-        _transfer(account, owner, 100);
-        users[account].membershipLevel++;
-    }
-
-    function Apply_Discount(uint256 itemId, address account) public view returns (uint256) {
-        require(itemId < itemsList.length, "Invalid item ID");
-        Item memory item = itemsList[itemId];
-        User memory user = users[account];
-
-        if (user.membershipLevel == 1) {
-            return item.cost * 90 / 100; // 10% discount
-        } else if (user.membershipLevel == 2) {
-            return item.cost * 80 / 100; // 20% discount
-        } else {
-            return item.cost;
-        }
     }
 }
